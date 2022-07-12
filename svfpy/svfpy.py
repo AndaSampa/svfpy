2dfc30cc3560961d859c199f79287da0d05b088e
Fernando Gomes <feromes@gmail.com>
2022-07-12 17:43:40 -0300

    DEv CAculation MEthod

--- svfpy/svfpy.py
+++ svfpy/svfpy.py
@@ -1,10 +1,12 @@
 # import argparse
 import rasterio
+from rasterio import warp
 import math
 from scipy.spatial.distance import euclidean
 from skimage.draw import line
 from skimage.util import view_as_windows
 import numpy as np
+from rasterio.enums import Resampling
 
 class SVF:
   def __init__(self, 
@@ -13,37 +15,18 @@ class SVF:
                kernel_size = 11,
                max_radius = None):
     self.mds_src = rasterio.open(mds_path)
-    self.mds = self.mds_src.read(1).astype('float16')
-    ## TODO
-    # PAD
-    # rasterio.pad(seila.mds, seila.mds_src.transform, 5, mode='reflect')[0]
     self.observer_height = observer_height
     self.kernel_size = self._is_kernel_size_odd(kernel_size)
     
     if max_radius is None:
       self.max_radius = max(self.mds_src.shape)
-      # self.max_radius = math.ceil(math.sqrt(self.mds_src.shape[0] ** 2 + self.mds_src.shape[-1] ** 2))
     else:
       self.max_radius = max_radius
-
-    self.pixel_max_size = math.ceil(self.max_radius / math.floor(self.kernel_size / 2) * max(self.mds_src.res))
-    self.downscale_times = math.ceil(math.log2(self.pixel_max_size / max(self.mds_src.res)) + 1)
-
-    self.view_as_windows = view_as_windows(self.mds, (self.kernel_size, self.kernel_size))
-
-## TODOO
-# Interagindo entre as outras resolucoes
-# faz a convooluçao em 4000X4000, 2000X2000, 1000X1000 
-# e depois expande com np.repeat() e vai concatenando
 
   def _is_kernel_size_odd(self, kernel_size):
     if int(kernel_size) % 2 == 0:
       raise ValueError("Kernel Size must be odd integer!")
     return int(kernel_size)
-
-  def rays(self):
-    kernel = self.view_as_windows
-    return kernel[:, :, self.fuse_lines()[:, :, 0], self.fuse_lines()[:, :, 1]]
 
   def external_coordinates(self):
     kernel_size = self.kernel_size
@@ -64,19 +47,6 @@ class SVF:
 
     return external_coords, fuse_angles, fuse_angles_range
 
-  def fuse_lines(self):
-    kernel_size = self.kernel_size
-    external_coords = self.external_coordinates()[0]
-    lines = []
-    center_point = (int(kernel_size//2), int(kernel_size//2))
-    for c in external_coords:
-        lines.append(line(*center_point, *c))
-    #     print(*center_point, *c)
-    #     break
-
-    fuse_lines = np.array([np.array(l).T for l in np.array(lines)])
-
-    return fuse_lines
 
 def distance_matrix(size=11):
   kernel_simple = np.ones((size, size), dtype='float16')
@@ -90,6 +60,63 @@ def distance_matrix(size=11):
           
   return distances
 
+def calculate(svf:SVF):
 
+  # Levantando os parâmetros para o calculo
+  pixel_max_size = math.ceil(svf.max_radius / math.floor(svf.kernel_size / 2) * max(svf.mds_src.res))
+  downscale_times = math.ceil(math.log2(pixel_max_size / max(svf.mds_src.res)) + 1)
 
+  # Abre o MDS, com PADs
+  mds =  svf.mds_src.read(1).astype('float16')
+  mdss, transformations = [], []
 
+  for i in np.arange(downscale_times):
+    mds, transform = warp.reproject(source=svf.mds_src.read(1),
+                                    src_transform=svf.mds_src.transform,
+                                    src_crs=svf.mds_src.crs,
+                                    dst_crs=svf.mds_src.crs,
+                                    dst_nodata=svf.mds_src.nodata,
+                                    dst_resolution=tuple([r * (2 ** i) for r in svf.mds_src.res]),
+                                    resampling=Resampling.max)
+
+    # Adiciona PAD
+    mds, transform = rasterio.pad(np.squeeze(mds), transform, svf.kernel_size // 2, mode='reflect')
+
+    print('rays ..')
+    windows = view_as_windows(mds, (svf.kernel_size, svf.kernel_size))
+
+    # print('kernel ..')
+    # fuse_lines = _fuse_lines(svf)
+    # list_windows = windows[:, :, fuse_lines[:, :, 0], fuse_lines[:, :, 1]]
+
+    ######################################################
+    ## TODO
+    ######################################################
+    # np.max(windows[np.tile(lines_mask[8], (4000, 4000,1,1))].reshape(4000,4000,6), axis=2)
+    #########################################################################################
+
+    mdss.append(np.squeeze(mds))
+    transformations.append(transform)
+
+    break
+  
+  return windows
+  # return mdss, transformations
+
+def _fuse_lines(svf:SVF):
+  # kernel_size = svf.kernel_size
+  external_coords = svf.external_coordinates()[0]
+  lines = []
+  center_point = (int(svf.kernel_size//2), int(svf.kernel_size//2))
+  
+  for c in external_coords:
+      lines.append(line(*center_point, *c))
+
+  fuse_lines = np.array([np.array(l).T for l in np.array(lines)])
+  
+  return fuse_lines
+
+def _rays(svf):
+  kernel = view_as_windows(svf.mds, (self.kernel_size, self.kernel_size))
+  return kernel
+  # return kernel[:, :, self.fuse_lines()[:, :, 0], self.fuse_lines()[:, :, 1]]
