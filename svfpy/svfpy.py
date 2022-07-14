c34ee7c173c38556f5783e15eb6a33feb6990b08
Fernando Gomes <feromes@gmail.com>
2022-07-14 16:23:17 -0300

    Almost there

--- svfpy/svfpy.py
+++ svfpy/svfpy.py
@@ -75,34 +75,46 @@ def calculate(svf:SVF):
   # Levantando os parâmetros para o calculo
   pixel_max_size = math.ceil(svf.max_radius / math.floor(svf.kernel_size / 2) * max(svf.mds_src.res))
   downscale_times = math.ceil(math.log2(pixel_max_size / max(svf.mds_src.res)) + 1)
-  itens_quantity_by_scale = ((svf.kernel_size//2) + 1) / 2
-  print(itens_quantity_by_scale)
+  itens_quantity_by_scale = int(((svf.kernel_size//2) + 1) / 2)
+  kernel_border_quantity = (svf.kernel_size * 4) - 4
+  pixels_per_angle = downscale_times * itens_quantity_by_scale + itens_quantity_by_scale
+  fuse_lines = _fuse_lines(svf)
+  # print(itens_quantity_by_scale)
 
   # Abre o MDS, com PADs
   mds =  svf.mds_src.read(1).astype('float16')
   mdss, transformations = [], []
 
-  # Cria um arquivo em disco para persistir os calculos
-  # result_temp = zarr.open(
-  #   '../tmp/result_temp.zarr', 
-  #   mode='w', 
-  #   shape=(svf.mds_src.width, 
-  #         svf.mds_src.height, 
-  #         (svf.kernel_size * 4) - 4, 
-  #         (downscale_times * itens_quantity_by_scale) + itens_quantity_by_scale), 
-  #   chunks=(1000,1000), 
-  #   dtype=np.float32
-  # )
+  # Arranjo da array de mascaras
+  masks = lines_mask(svf)
+  a_masks = np.argwhere(masks).reshape(kernel_border_quantity,itens_quantity_by_scale * 2,3)[:,:,1:]
 
-  # result_temp = zarr.zeros((4000,4000,40,36))
+  # Cria um arquivo em disco para persistir os calculos
+  result_temp = zarr.open(
+    '../tmp/result_temp.zarr', 
+    mode='a', 
+    shape=(svf.mds_src.width, 
+          svf.mds_src.height, 
+          kernel_border_quantity, 
+          pixels_per_angle), 
+    chunks=(50,50), 
+    dtype=np.float16
+  )
+
+  distance_rows = np.zeros((kernel_border_quantity, pixels_per_angle), dtype='float16')
+  result_temp = zarr.zeros((svf.mds_src.width, svf.mds_src.height,kernel_border_quantity,pixels_per_angle))
 
   for i in np.arange(downscale_times):
+
+    resolution = tuple([r * (2 ** i) for r in svf.mds_src.res])
+
+    # Calcula arquivo
     mds, transform = warp.reproject(source=svf.mds_src.read(1),
                                     src_transform=svf.mds_src.transform,
                                     src_crs=svf.mds_src.crs,
                                     dst_crs=svf.mds_src.crs,
                                     dst_nodata=svf.mds_src.nodata,
-                                    dst_resolution=tuple([r * (2 ** i) for r in svf.mds_src.res]),
+                                    dst_resolution=resolution,
                                     resampling=Resampling.max)
 
     # Adiciona PAD
@@ -111,39 +123,39 @@ def calculate(svf:SVF):
 
     # Forma janelas do tamanho dos Kernels definidos
     windows = view_as_windows(mds, (svf.kernel_size, svf.kernel_size))
+    points = np.tile(windows[:,:, svf.kernel_size//2, svf.kernel_size//2].reshape(mds.shape[0] - svf.kernel_size + 1, mds.shape[1] - svf.kernel_size + 1,1,1), (1,1,kernel_border_quantity,1))
     
-    # Adicionar as linhas no arquivo de resultados temporarios
-    masks = lines_mask(svf)
-    for k, l in enumerate(masks):
-      # result_temp[:, :, k, 0:6] = windows[np.tile(l, (4000, 4000,1,1))].reshape(4000,4000,6)
-      seila = windows[:, :, masks[0,:,:].reshape(svf.kernel_size, svf.kernel_size)]
+    # Adicionar as linhas no arquivo de resultados temporarios        
+    if i == 0:
+      rows = windows[:, :, a_masks[:, :, 0], a_masks[:, :, 1]]
+      index = (0, itens_quantity_by_scale * 2)
+      print(index)
+      # Array de distancias em linhas
+      distance_rows[:, index[0]:index[1]] = distance_matrix(size=svf.kernel_size)[fuse_lines[:, :, 0], fuse_lines[:, :, 1]][:, :] * resolution[0]
+      # result_temp[:, :, :, index[0]:index[1]] = rows
+    else:
+      index = (int(i * itens_quantity_by_scale) + itens_quantity_by_scale, int(i * itens_quantity_by_scale) + itens_quantity_by_scale * 2)
+      print(index)
+      # Array de distancias em linhas
+      distance_rows[:, index[0]:index[1]] = distance_matrix(size=svf.kernel_size)[fuse_lines[:, :, 0], fuse_lines[:, :, 1]][:, 3:6] * resolution[0]
+      windows = np.tile(windows, (2**i, 2**i, 1, 1))
+      points = np.tile(points, (2**i, 2**i, 1, 1))[0:svf.mds_src.width, 0:svf.mds_src.width, :, :]
+      # break
+      rows = windows[:, :, a_masks[:, :, 0], a_masks[:, :, 1]][0:svf.mds_src.width, 0:svf.mds_src.width, :, itens_quantity_by_scale:]
 
-    # Calcular o angulo para cada pixel
-    print('.. difff')
-    h_diff = np.subtract(windows, np.expand_dims(windows[:,:,svf.kernel_size//2,svf.kernel_size//2], axis=(2,3)))
-    print(' .. angles')
-    angles = np.arctan2(h_diff, (distance_matrix() * 0.5)) 
+    # Adiciona o calculo do angulo aos resultados temporários
+    result_temp[:, :, :, index[0]:index[1]] = np.arctan2(rows - points, distance_rows[:, index[0]:index[1]])
 
-    # Calcula o angulo máximo para cada fuso
-    max_angles = np.zeros((windows.shape[0], windows.shape[1], (svf.kernel_size * 4) - 4), dtype=np.float16)
-    for k, l in enumerate(masks):
-      print('.')
-      max_angles[:, :, l] = np.max(angles[:, :, masks[0,:,:]], axis=2)
-
-
-    ######################################################
     ## TODO
-    ######################################################
-    # np.max(windows[np.tile(lines_mask[8], (4000, 4000,1,1))].reshape(4000,4000,6), axis=2)
-    #########################################################################################
+    # Achar o angulo máximo ou ArgMax
+    # np.max(result_temp[:500, :500, :, 1:], axis=3)
 
-    mdss.append(np.squeeze(mds))
-    transformations.append(transform)
+    # Persistir o dado
 
-    break
-  return angles
-  # return windows
-  # return mdss, transformations
+
+
+  return result_temp
+
 
 
 def _fuse_lines(svf:SVF):
